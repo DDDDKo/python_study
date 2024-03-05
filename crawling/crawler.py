c423e0c11a69d9d2fbbf2c6ab8ff8bbde97d54dd
DDDDKo <to1583@naver.com>
2024-03-05 16:48:36 +0900

    2024-03-05 16:50

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -54,8 +54,58 @@ search_input.send_keys(Keys.ENTER)
 time.sleep(0.3)
 
 news_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(8) > a')
-# search_input.send_keys(Keys.ENTER)
+# news_button.send_keys(Keys.ENTER)
 time.sleep(0.3)
 
 ActionChains(driver).click(news_button).perform()
-time.sleep(3)
+time.sleep(0.3)
+
+news_title_elements = driver.find_elements(By.CLASS_NAME, 'news_tit')
+time.sleep(0.3)
+
+for news_title_element in news_title_elements:
+    # title = news_title_element.text
+    title = news_title_element.get_attribute('title')
+    print(title)
+time.sleep(0.3)
+
+driver.back()
+time.sleep(0.3)
+
+image_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(1) > a')
+time.sleep(0.3)
+
+ActionChains(driver).click(image_button).perform()
+time.sleep(0.3)
+
+image_elements = driver.find_elements(By.CLASS_NAME, '_fe_image_tab_content_thumbnail_image')
+time.sleep(0.3)
+
+image_list = []
+
+for image_element in image_elements:
+    image_src = image_element.get_attribute('src')
+    print(image_src)
+    image_list.append(image_src)
+time.sleep(0.3)
+
+# 파이썬으로 폴더 생성//
+import os
+
+FOLDER_PATH = r'./images/'
+
+if not os.path.isdir(FOLDER_PATH):
+    os.mkdir(FOLDER_PATH)
+
+# 파이썬으로 폴더 생성//
+
+# 파이썬으로 이미지 url 파일 다운로드//
+from urllib.request import urlretrieve
+
+number = 1
+
+for image_src in image_list:
+    urlretrieve(image_src, FOLDER_PATH + f'{number}.png')
+    number += 1
+    time.sleep(0.5)
+# 파이썬으로 이미지 url 파일 다운로드//
